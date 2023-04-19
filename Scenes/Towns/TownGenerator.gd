4dc365462e959d139dd05d72c6e204072f5948da
itaden1 <ethanshearer1@gmail.com>
2023-04-19 17:06:27 +1000

    add padding and room size check to BPT

--- Scenes/Towns/TownGenerator.gd
+++ Scenes/Towns/TownGenerator.gd
@@ -3,28 +3,28 @@ extends Spatial
 const bpt_generator = preload("res://Scripts/Generators/BinaryPartitionTree.gd")
 const basic_block = preload("res://Scenes/Towns/BuildingBlocks/Generic/Block.tscn")
 
-const offset := 5
+const offset := 6
 
 func generate_town(params: Dictionary):
-	var width: float = 10
-	var height: float = 10
+	var width: float = 12
+	var height: float = 12
 	var bpt = bpt_generator.new()
 	var tree = bpt.partition_rect(
 		Rect2(Vector2(0, 0), Vector2(width, height)),
-		4,
+		3,
 		6,
-		2
+		2,
+		[3, 2]
 	)
 	for b in tree.keys():
 		if tree[b].size() <= 0:
-			print(b)
 			build_house(b)
 
 
 func build_house(rect: Rect2):
 
-	for x in range(rect.position.x, rect.position.x + rect.size.x, offset):
-		for y in range(rect.position.y, rect.position.y + rect.size.y, offset):
+	for x in range(rect.position.x, rect.position.x + rect.size.x +1):
+		for y in range(rect.position.y, rect.position.y + rect.size.y +1):
 			var block = basic_block.instance()
 
 			add_child(block)
--- Scripts/Generators/BinaryPartitionTree.gd
+++ Scripts/Generators/BinaryPartitionTree.gd
@@ -20,11 +20,23 @@ func partition_rect(
 		return
 
 	var partition_directions = ["v", "h"]
-	var direction = partition_directions[Rng.get_random_range(0,1)]
+	var padd = 1
+	if current_partition <= padding.size() - 1:
+		padd = padding[current_partition]
 	
+	# check if further split possible
+	var direction = partition_directions[Rng.get_random_range(0,1)]
+	if rect.size.x - padd <= min_room_size and rect.size.y - padd <= min_room_size:
+		rects.append(rect)
+		return
+	elif rect.size.x - padd <= min_room_size:
+		direction = 'h'
+	elif rect.size.y - padd <= min_room_size:
+		direction = 'v'
+
 	if direction == "v":
-		var left_partition_width = Rng.get_random_range(min_room_size, rect.size.x - min_room_size)
-		var right_partition_width = rect.size.x - left_partition_width
+		var left_partition_width = Rng.get_random_range(min_room_size, rect.size.x - min_room_size - padd)
+		var right_partition_width = rect.size.x - left_partition_width - padd
 		var new_rect_1 = Rect2(Vector2(rect.position.x, rect.position.y), Vector2(left_partition_width, rect.size.y))
 		var new_rect_2 = Rect2(Vector2(rect.size.x - right_partition_width, rect.position.y), Vector2(right_partition_width, rect.size.y))
 
@@ -50,8 +62,8 @@ func partition_rect(
 		)
 
 	else:
-		var top_partition_height = Rng.get_random_range(min_room_size, rect.size.y - min_room_size)
-		var bottom_partition_height = rect.size.y - top_partition_height
+		var top_partition_height = Rng.get_random_range(min_room_size, rect.size.y - min_room_size - padd)
+		var bottom_partition_height = rect.size.y - top_partition_height - padd
 		var new_rect_1 = Rect2(Vector2(rect.position.x, rect.position.y), Vector2(rect.size.x, top_partition_height))
 		var new_rect_2 = Rect2(Vector2(rect.position.x, rect.size.y - bottom_partition_height), Vector2(bottom_partition_height, rect.size.y))
 
